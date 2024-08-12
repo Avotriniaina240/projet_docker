0d1f138b6fdfa8d9df66685c7752d8aa399d8357
fiacrednj <fiacrednj@gmail.com>
2024-08-12 11:42:36 +0300

    Commit avotra

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="title">Informations Personnelles</h1>
+        <div className="info">
+            <p><strong>Nom :</strong> RABOTOSON</p>
+            <p><strong>Prénom :</strong> Avotriniaina Flavien Rodolphe</p>
+            <p><strong>Matricule :</strong> 3774</p>
+            <p><strong>Niveau :</strong> M2</p>
+            <p><strong>Mention :</strong> M2I</p>
+        </div>
     </div>
   );
 }
